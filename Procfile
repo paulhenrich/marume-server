cec28f2cf0138b8d1e884c7bca93400f5fe57000
Paul Henrich <paul@henrich.me>
2015-02-14 18:51:30 -0500

    lein new

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/marume-standalone.jar clojure.main -m marume.web
---
+++ test/marume/web_test.clj
@@ -0,0 +1,6 @@
+(ns marume.web-test
+  (:require [clojure.test :refer :all]
+            [marume.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
